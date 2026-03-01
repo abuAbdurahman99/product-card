c45092f1dee36935d9128aff7b81629956edb77d
Adil <adiltokushev240199@gmail.com>
2026-03-01 13:07:32 +0600

    add script.js

---
+++ cript.js
@@ -0,0 +1,174 @@
+[33mcommit 030f775be267e34447732321fdb42a433696df62[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mfeature/04-homework[m[33m, [m[1;31morigin/homework-3[m[33m, [m[1;31morigin/feature/04-homework[m[33m)[m
+Author: Adil <adiltokushev240199@gmail.com>
+Date:   Sat Feb 28 17:03:07 2026 +0600
+
+    homework-4
+
+index.html
+
+[33mcommit 97e0d2323b7a8eabaf1e1eeda7aea7f4dd65ec36[m
+Author: Adil <adiltokushev240199@gmail.com>
+Date:   Sat Feb 28 16:52:15 2026 +0600
+
+    homework-4
+
+index.html
+script.js
+style.css
+
+[33mcommit c5bdb68bc32be75c923b720f846d78ebdc8b0684[m
+Author: Adil <adiltokushev240199@gmail.com>
+Date:   Tue Feb 24 07:18:25 2026 +0600
+
+    fixed
+
+index.html
+
+[33mcommit 4239c42f74532aa04bccb7b1852b88fcf3376b7c[m
+Author: Adil <adiltokushev240199@gmail.com>
+Date:   Mon Feb 23 19:14:51 2026 +0600
+
+    fixed
+
+script.js
+
+[33mcommit eb86773cd0ab64428650308aa76c02366a33ca89[m
+Author: Adil <adiltokushev240199@gmail.com>
+Date:   Mon Feb 23 19:14:14 2026 +0600
+
+    fixed
+
+index.html
+
+[33mcommit 057bc79449d644aac5d9372023dd19d777cddc87[m
+Author: Adil <adiltokushev240199@gmail.com>
+Date:   Mon Feb 23 18:01:34 2026 +0600
+
+    fixed
+
+index.html
+
+[33mcommit 9e52e02a19ae7051f4d6f98de57f54257bb1a2df[m
+Author: Adil <adiltokushev240199@gmail.com>
+Date:   Mon Feb 23 09:09:10 2026 +0600
+
+    fixed
+
+index.html
+style.css
+
+[33mcommit ad0221cc198bcd7d5168adf434c0b711adbb730b[m
+Author: Adil <adiltokushev240199@gmail.com>
+Date:   Mon Feb 23 05:49:34 2026 +0600
+
+    fixed
+
+style.css
+
+[33mcommit efaefe482492bc2862b61ed1fdb76b4222776072[m
+Author: Adil <adiltokushev240199@gmail.com>
+Date:   Tue Feb 17 19:16:23 2026 +0600
+
+    fixed
+
+index.html
+
+[33mcommit 11f76b3d837c27bdbc0c975a1ff25e36d2736e6a[m
+Author: Adil <adiltokushev240199@gmail.com>
+Date:   Mon Feb 16 22:42:47 2026 +0600
+
+    Исправил ошибки (3)
+
+images/bg-dandelions.png
+images/hydrating-mask.png
+index.html
+style.css
+
+[33mcommit eb79ee8fbb87aaa3ee9f25025fca90e18e944683[m
+Author: Adil <adiltokushev240199@gmail.com>
+Date:   Sun Feb 15 21:44:42 2026 +0600
+
+    hw
+
+index.html
+
+[33mcommit 241bee1671a48d57175e842faa179e2b3ebf2a30[m
+Author: Adil <adiltokushev240199@gmail.com>
+Date:   Sun Feb 15 21:40:01 2026 +0600
+
+    homework #3
+
+images/background.png
+images/face-wash.png
+images/gift-set-1.png
+images/gift-set-5.png
+images/hydrating-mask .png
+index.html
+style.css
+
+[33mcommit abde4e6d3e88d4ddb1a5865d89a1f7af1d5f268d[m[33m ([m[1;31morigin/homework-2[m[33m, [m[1;32mhomework-2[m[33m)[m
+Author: Adil <adiltokushev240199@gmail.com>
+Date:   Sat Feb 14 12:40:39 2026 +0600
+
+    delete line
+
+index.html
+
+[33mcommit 594cdf6dfccedbab9736c2630fcfbe7c84cd9057[m
+Author: Adil <adiltokushev240199@gmail.com>
+Date:   Thu Feb 12 22:38:07 2026 +0600
+
+    product card
+
+README.md
+images/moisturizing-mousse.png
+index.html
+
+[33mcommit b9bc6681218207f49d3897f285ac81c5e13efaef[m[33m ([m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m, [m[1;32mhomework-1[m[33m)[m
+Merge: 252229d a3540e8
+Author: abuAbdurahman99 <adiltokushev240199@gmail.com>
+Date:   Mon Feb 9 18:14:27 2026 +0600
+
+    Merge pull request #2 from abuAbdurahman99/task
+    
+    name
+
+[33mcommit a3540e83906a16af6762d049b800f3f7dbdba538[m[33m ([m[1;31morigin/task[m[33m, [m[1;32mtask[m[33m)[m
+Author: Adil <adiltokushev240199@gmail.com>
+Date:   Mon Feb 9 18:14:00 2026 +0600
+
+    name
+
+README.md
+
+[33mcommit 252229de2e6a8bc298841fbd4f5edbad5445e5e8[m
+Author: Adil <adiltokushev240199@gmail.com>
+Date:   Mon Feb 9 18:12:49 2026 +0600
+
+    name
+
+README.md
+
+[33mcommit fdb5dea8f094557bed95985eccbdd250ded4cb66[m
+Author: Adil <adiltokushev240199@gmail.com>
+Date:   Mon Feb 9 18:10:23 2026 +0600
+
+    name
+
+README.md
+
+[33mcommit 339998d8b59fd8eb3b3341f1cb7b90ee045f0b73[m[33m ([m[1;31morigin/homework-1[m[33m)[m
+Author: Adil <adiltokushev240199@gmail.com>
+Date:   Sun Feb 8 23:27:12 2026 +0600
+
+    update readme file
+
+README.md
+
+[33mcommit 8ec703d79d3798c9ba63c6914df197095eec5ab5[m
+Author: abuAbdurahman99 <adiltokushev240199@gmail.com>
+Date:   Sun Feb 8 23:16:56 2026 +0600
+
+    Initial commit
+
+README.md
